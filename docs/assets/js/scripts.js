ef0502619cbc75d9f6cf1822ef3add7f58c6bb98
thiagomurgia <tzoppeim@live.com>
2023-08-16 22:58:46 -0300

    fix: searchButton without value redirect to all profiles

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -5,5 +5,9 @@ searchButton.addEventListener('click', (e) => {
   e.preventDefault();
   const searchValue = searchInput.value;
   const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
+  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
+  if(searchValue===''){
+    window.open(defaultUrl, '_blank')
+  }
   window.open(url, '_blank');
 })
