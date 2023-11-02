8de48a053978518ee4e8882d8346ed57e7cdea8e
valdir-alves3000 <valdiralves3000@gmail.com>
2023-11-02 20:15:37 -0300

    feat: Implementation of Dynamic Suggestions and Enhancement of Search Experience

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,17 +1,73 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
+const suggestionsModal = document.getElementById("suggestionsModal");
+const suggestionsList = document.getElementById("suggestionsList");
 
-searchButton.addEventListener('click', (e) => {
+async function fetchAndDisplaySuggestions() {
+  try {
+    const apiUrl =
+      "https://api.github.com/repos/digitalinnovationone/dio-lab-open-source/contents/community";
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const mdFiles = data
+      .filter((item) => item.type === "file" && item.name.endsWith(".md"))
+      .map((item) => item.name.replace(".md", ""));
+
+    searchInput.addEventListener("input", updateSuggestions);
+
+    function updateSuggestions() {
+      const inputValue = searchInput.value.toLowerCase();
+      const filteredSuggestions = mdFiles.filter((suggestion) =>
+        suggestion.toLowerCase().includes(inputValue)
+      );
+      displaySuggestions(filteredSuggestions);
+    }
+
+    function displaySuggestions(filteredSuggestions) {
+      if (filteredSuggestions.length > 0) {
+        suggestionsModal.style.display = "block";
+      } else {
+        suggestionsModal.style.display = "none";
+      }
+
+      suggestionsList.innerHTML = "";
+
+      for (let i = 0; i < filteredSuggestions.length; i++) {
+        const suggestion = filteredSuggestions[i];
+        const suggestionItem = document.createElement("li");
+        suggestionItem.textContent = suggestion;
+        suggestionItem.addEventListener("click", () => {
+          searchInput.value = suggestion;
+          suggestionsModal.style.display = "none";
+        });
+
+        suggestionsList.appendChild(suggestionItem);
+      }
+    }
+  } catch (error) {
+    console.error("Erro ao buscar dados da API:", error);
+  }
+}
+
+fetchAndDisplaySuggestions();
+
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
   const searchValue = searchInput.value;
-  if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
-    return;
+  if (searchValue === "") {
+    alert("Por favor, insira um termo de pesquisa.");
+  } else {
+    const url = `https://github.com/digitalinnovationone/dio-lab-open-source/blob/main/community/${encodeURIComponent(
+      searchValue
+    )}.md`;
+
+    window.open(url, "_blank");
   }
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
-  if(searchValue===''){
-    window.open(defaultUrl, '_blank')
+});
+
+document.addEventListener("click", (e) => {
+  if (!suggestionsModal.contains(e.target) && e.target !== searchInput) {
+    suggestionsModal.style.display = "none";
   }
-  window.open(url, '_blank');
-})
+});
