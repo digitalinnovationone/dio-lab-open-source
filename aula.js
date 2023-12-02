7f1c4c8f00a5a32597d6f732ab9d84f47a718335
Roger Yan <152312802+roger4yan@users.noreply.github.com>
2023-12-02 09:41:01 -0300

    Delete aula.js

--- aula.js
+++
@@ -1,9 +0,0 @@
-
-
-const precoCombustivel = 5.79;
-const kmPorLitros = 10;
-const distanciaEmKm = 100;
-
-const litrosConsumido = distanciaEmKm / kmPorLitros;
-const valorGasto = litrosConsumido * precoCombustivel;
-console.log(valorGasto);
