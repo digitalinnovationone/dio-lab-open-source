a5811235cf30389e3168843222ca48181f785403
Dânison dos Santos <danison.santos00@gmail.com>
2024-03-13 19:13:33 -0300

    feat: adicionando validacao de existencia de usuario

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,17 +1,59 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
 
-searchButton.addEventListener('click', (e) => {
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
-  const searchValue = searchInput.value;
-  if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
+  const searchValue = searchInput.value.trim();
+  if (searchValue === "") {
+    alert("Por favor, insira um termo de pesquisa."); // Exibir um alerta se o campo estiver vazio
     return;
   }
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
-  if(searchValue===''){
-    window.open(defaultUrl, '_blank')
-  }
-  window.open(url, '_blank');
-})
+
+  verifyUsernameExists(searchValue);
+});
+
+function verifyUsernameExists(searchValue) {
+  const urlApi = `https://api.github.com/repos/digitalinnovationone/dio-lab-open-source/git/trees/81f84c8cabb62b4c7138e77283603305cd526812`;
+  const urlReadMe = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(
+    searchValue
+  )}.md`;
+
+  fetch(urlApi)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else if (response.status === 404) {
+        console.log("O arquivo não existe.");
+      } else {
+        console.error("Erro:", response.status);
+      }
+    })
+    .then((data) => {
+      return verify(data.tree, searchValue);
+    })
+    .then((exist) => {
+      if (exist) window.open(urlReadMe, "_blank");
+      else treatMessageError();
+    })
+    .catch((error) => console.error(error));
+}
+
+function verify(tree, searchValue) {
+  searchValue += ".md";
+
+  return new Promise((resolve) => {
+    tree.forEach((arq) => {
+      if (arq.path == searchValue) {
+        resolve(true);
+      }
+    });
+    resolve(false); // Se o arquivo não for encontrado
+  });
+}
+
+function treatMessageError() {
+  document.getElementById("alert-message").style.display = "block";
+}
+document.getElementById("searchInput").addEventListener("input", () => {
+  document.getElementById("alert-message").style.display = "none";
+});
