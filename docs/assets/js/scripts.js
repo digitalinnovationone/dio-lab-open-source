0f303224b8361a53548debe6f4991bd0dcab28a7
Jonathan Silva <40859852+jeas560@users.noreply.github.com>
2024-04-02 16:52:43 +0000

    script to suggesting a random value in `searchInput`

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,6 +1,24 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 
+document.addEventListener("DOMContentLoaded", function () {
+  const apiUrl =
+    "https://api.github.com/repos/digitalinnovationone/dio-lab-open-source/contents/community";
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const fileNames = data.map((file) => file.name);
+
+      const randomIndex = Math.floor(Math.random() * fileNames.length);
+      const randomFileName = fileNames[randomIndex];
+
+      searchInput.value = randomFileName;
+    })
+    .catch((error) => {
+      console.error("Error fetching file list:", error);
+    });
+});
+
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
   const searchValue = searchInput.value;
