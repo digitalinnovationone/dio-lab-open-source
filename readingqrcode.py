d942bde88373cf7ac9b92b1f89377166b61371b1
Gabis <gabrielabcarro@gmail.com>
2024-08-27 12:14:21 -0300

    Delete readingqrcode.py

--- readingqrcode.py
+++
@@ -1,8 +0,0 @@
-from pyzbar.pyzbar import decode 
-from PIL import Image 
-
-img = Image.open('C:/Users/gabriela.carro/Documents/Gabis/Challenge/myredqrcode.png')
-
-result = decode(img)
-
-print(result)
