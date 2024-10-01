5883d17c3ff8bb374dcf735c4059a537a194f449
Hendreu <89034425+Hendreu@users.noreply.github.com>
2024-10-01 20:36:39 -0300

    Add files via upload

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,17 +1,20 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById("searchInput");
+const searchButton = document.getElementById("searchButton");
 
-searchButton.addEventListener('click', (e) => {
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
   const searchValue = searchInput.value;
-  if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
+  if (searchValue === "") {
+    alert("Por favor, insira um termo de pesquisa."); // Exibir um alerta se o campo estiver vazio
     return;
   }
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
-  if(searchValue===''){
-    window.open(defaultUrl, '_blank')
+  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(
+    searchValue
+  )}.md`;
+  const defaultUrl =
+    "https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community";
+  if (searchValue === "") {
+    window.open(defaultUrl, "_blank");
   }
-  window.open(url, '_blank');
-})
+  window.open(url, "_blank");
+});
