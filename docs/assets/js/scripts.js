ecd5c0a5c7ae374d3edc93c8141a0f5aa60913e5
degoNDL <diegoclebson32@gmail.com>
2025-02-16 16:29:39 -0300

    remoção de redundâncias e otimização

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -3,15 +3,15 @@ const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const searchValue = searchInput.value;
+  const searchValue = searchInput.value.trim();
+  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community';
+
   if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
+    alert('Por favor, insira um termo de pesquisa.');
+    window.open(defaultUrl, '_blank');
     return;
   }
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
-  if(searchValue===''){
-    window.open(defaultUrl, '_blank')
-  }
+
+  const url = `${defaultUrl}/${encodeURIComponent(searchValue)}.md`;
   window.open(url, '_blank');
-})
+});
