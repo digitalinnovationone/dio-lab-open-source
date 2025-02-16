a4fde4a419f5bc9bf000e29964ff0ad34fa0a42f
degoNDL <diegoclebson32@gmail.com>
2025-02-16 16:38:59 -0300

    aa

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -3,15 +3,15 @@ const searchButton = document.getElementById('searchButton');
 
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const searchValue = searchInput.value.trim();
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community';
-
+  const searchValue = searchInput.value;
   if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.');
-    window.open(defaultUrl, '_blank');
+    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
     return;
   }
-
-  const url = `${defaultUrl}/${encodeURIComponent(searchValue)}.md`;
+  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
+  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
+  if(searchValue===''){
+    window.open(defaultUrl, '_blank')
+  }
   window.open(url, '_blank');
-});
+})
