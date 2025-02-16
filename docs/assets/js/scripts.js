22f3cad97ec6493eb5dbed02872c1e7a066b26cd
Vinicius Koiti <viniciusnakahara@gmail.com>
2025-02-16 11:37:35 -0300

    Criando o um editor com comportamento de buscar dentro de community

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,5 +1,8 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
+const textarea = document.getElementById('markdown');
+const output = document.getElementById('output');
+const convertButton = document.getElementById('convertButton');
 
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
@@ -14,4 +17,26 @@ searchButton.addEventListener('click', (e) => {
     window.open(defaultUrl, '_blank')
   }
   window.open(url, '_blank');
+})
+
+textarea.addEventListener('input', (e) => {
+  e.preventDefault();
+  const converter = new showdown.Converter({
+    simpleLineBreaks: true,
+    strikethrough: true,
+    tables: true,
+    ghCompatibleHeaderId: true,
+    emoji: true,
+    disableForced4SpacesIndentedSublists: true, 
+    literalMidWordUnderscores: true 
+  });
+
+  function updatePreview() {
+    const markdownText = textarea.value;
+    const html = converter.makeHtml(markdownText);
+    output.innerHTML = html;
+  }
+
+  updatePreview();
+                      
 })
