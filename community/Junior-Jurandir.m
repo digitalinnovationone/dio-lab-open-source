fceb7ec104e8a0a79a5e276d45773ba170aaadd2
Junior-Jurandir <100539777+Junior-Jurandir@users.noreply.github.com>
2025-03-02 16:29:14 -0300

    Create Junior-Jurandir.md
    Adição do meu currículo em Markdown

---
+++ community/Junior-Jurandir.m
@@ -0,0 +1,112 @@
+# Jurandir Junior
+**Analista de Machine Learning**
+
+---
+## Técnologias
+
+**Python** | **Linux** | Arduino | R | SQL | Flutter | Power BI | Machine Learning | Data Science | Data Visualization
+
+
+---
+
+## Sobre Mim
+
+Com três anos de experiência, possuo uma base sólida em resolução de problemas, criatividade e comunicação, habilidades essenciais para o desenvolvimento de soluções inovadoras em Machine Learning. Minha trajetória inclui a criação da startup Moonlight Ltda. e atuação como Analista de Suporte na Prefeitura de Santana de Parnaíba, onde desenvolvi habilidades técnicas e estratégicas. Foi lá também que criei uma ferramenta inteligente baseada no modelo LLaMA da Meta. Com conhecimentos em Python, automação e análise de dados, busco aplicar conceitos de IA em cenários reais, contribuindo para a integração de soluções inovadoras e eficientes.
+
+---
+
+## Informações Pessoais
+
+- **Nome:** Jurandir Batista de Souza Junior
+- **Email:** Jura15012001@gmail.com
+- **LinkedIn:** [Jurandir Jr](https://www.linkedin.com/in/jurandir-jr)
+- **GitHub:** [Jurandir Jr](https://github.com/Junior-Jurandir)
+- **Telefone:** (11) 98712-3161 / (11) 97248-0874
+- **Endereço:** Rua Aracaju 20, Santana de Parnaíba - SP
+
+---
+
+## Resumo
+
+**2023–2025**  
+**Analista de Suporte Técnico**  
+**LIDERANÇA · Prefeitura de Santana de Parnaíba**  
+- Atuei na gestão do setor de Service Desk da Prefeitura de Santana de Parnaíba, desenvolvendo habilidades para resolução de problemas, governança de TI, trabalhando para a certificação do setor na ISO 9001 e atuando ativamente na capacitação dos funcionários do setor.
+
+**2022–2023**  
+**Web Designer**  
+**EMPREENDIMENTO · Moonlight Ltda.**  
+- Empreendi como Web Designer, criando páginas de venda para empreendedores locais e facilitando a introdução deles ao ambiente digital. Encerrei o projeto ao ser chamado para trabalhar na prefeitura, mas indiquei uma empresa de confiança para dar continuidade ao atendimento dos clientes.
+
+---
+
+## Formação Acadêmica
+
+**2019**  
+**Ensino Médio**  
+**C.M. Dr. Paulo Octávio Botelho · SP**
+
+**2024**  
+**Ciência da Computação**  
+**UNIP · SP**
+
+---
+
+## Habilidades
+
+- **Programação:** Python, R, SQL, Arduino
+- **Criatividade**
+- **Gestão de TI**
+- **Suporte Técnico**
+- **ISO 9001**
+- **Treinamento de Pessoal**
+
+---
+
+## Experiência Profissional
+
+**2023–2024**  
+**Analista de Suporte Técnico**  
+**LIDERANÇA · Prefeitura de Santana de Parnaíba**  
+- Suporte à equipe de NL desenvolvimento de materiais para a base de conhecimento, análise de indicadores e apoio na avaliação de colaboradores, visando a melhoria contínua dos processos e da eficiência no atendimento.
+
+**2019**  
+**Web Designer**  
+**EMPREENDIMENTO · Moonlight Ltda.**  
+- Confeccionar páginas web para marketing pessoal e corporativo, prestar suporte técnico a essas páginas e gerenciar campanhas de tráfego pago.
+
+---
+
+## Últimos Cursos e Publicações
+
+**2024**  
+- **Dominando LLMs com Langchain**
+- **Asimov: IA Generativa especializada em Suporte técnico**
+
+**2025**  
+- **Formação completa em IA e Machine Learning**
+
+---
+
+## Palestras Ministradas
+
+**Fev. 2024**  
+- **Soft skills para suporte técnico**  
+  Prefeitura de Santana de Parnaíba, Fev. 2024.
+
+---
+
+## Interesses Pessoais
+
+Em meu tempo livre gosto de desenvolver projetos de automação ou IA, que me auxiliem no meu trabalho, além de passar um tempo de qualidade com minha noiva, minha família e amigos.
+
+---
+
+## Interesses Profissionais
+
+- **ITIL**
+- **Suporte Técnico**
+- **Linux**
+- **Inteligência Artificial**
+- **Atendimento Humanizado**
+- **Gestão de Pessoas**
