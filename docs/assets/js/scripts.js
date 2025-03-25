dfc3b2d21b509062aaf3ad99d6205084791f37e2
Randerson Costa <randersoncosta0339@gmail.com>
2025-03-25 13:01:57 -0300

    inclusão do botão dark-mode

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,12 +1,43 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const searchValue = searchInput.value.trim(); // Remover espaços extras
-
+  const searchValue = searchInput.value;
   if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.');
+    alert('Por favor, insira um termo de pesquisa.'); 
     return;
   }
-
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`;
+  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
+  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
+  if(searchValue===''){
+    window.open(defaultUrl, '_blank')
+  }
   window.open(url, '_blank');
-});
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleThemeBtn = document.getElementById("toggleTheme");
+  
+    
+    if (localStorage.getItem("theme") === "dark") {
+      document.body.classList.add("dark-mode");
+      toggleThemeBtn.textContent = "☀️";
+    } else {
+      toggleThemeBtn.textContent = "🌙"; 
+    }
+  
+    
+    toggleThemeBtn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+  
+      if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        toggleThemeBtn.textContent = "☀️"; 
+      } else {
+        localStorage.setItem("theme", "light");
+        toggleThemeBtn.textContent = "🌙"; 
+      }
+    });
+  });
+  
