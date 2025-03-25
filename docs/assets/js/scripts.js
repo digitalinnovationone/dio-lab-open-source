9925227be959604c1fba26c456ec974e750fe204
Randerson Costa <randersoncosta0339@gmail.com>
2025-03-25 11:37:10 -0300

    Correção de bug e Melhoria

--- docs/assets/js/scripts.js
+++ docs/assets/js/scripts.js
@@ -1,17 +1,12 @@
-const searchInput = document.getElementById('searchInput');
-const searchButton = document.getElementById('searchButton');
-
 searchButton.addEventListener('click', (e) => {
   e.preventDefault();
-  const searchValue = searchInput.value;
+  const searchValue = searchInput.value.trim(); // Remover espaços extras
+
   if (searchValue === '') {
-    alert('Por favor, insira um termo de pesquisa.'); // Exibir um alerta se o campo estiver vazio
+    alert('Por favor, insira um termo de pesquisa.');
     return;
   }
-  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`; 
-  const defaultUrl = 'https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community'
-  if(searchValue===''){
-    window.open(defaultUrl, '_blank')
-  }
+
+  const url = `https://github.com/elidianaandrade/dio-lab-open-source/tree/main/community/${encodeURIComponent(searchValue)}.md`;
   window.open(url, '_blank');
-})
+});
